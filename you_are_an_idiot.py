c66d2c002dee1f1540aabc24acbbcc48d5e6076d
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2021-12-28 14:44:36 -0800

    idiot

--- you_are_an_idiot.py
+++ you_are_an_idiot.py
@@ -2,7 +2,7 @@ from tkinter import *
 import threading
 import random
 from time import sleep
-import pygame.mixer
+import playsound
 import os
 cwd = os.getcwd()
 
@@ -16,16 +16,24 @@ def movethread():
             break
         sleep(1)
 
-pygame.mixer.init()
-pygame.mixer.music.load("idiot.mp3")
-pygame.mixer.music.play(loops=-1)
+def soundthread():
+    global iss
+    while iss:
+        if iss:
+            playsound.playsound("idiot.mp3")
+            sleep(3)
+        else:
+            break
+
 root = Tk()
+iss = True
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 lbl = Label(root,text="     You are an idiot!      \n")
 lbl.pack()
 threading.Thread(target=movethread,name="moveThread").start()
+threading.Thread(target=soundthread,name="soundThread").start()
 root.mainloop()
-pygame.mixer.quit()
+iss = False
 for i in range(5):
     os.startfile("you_are_an_idiot.exe")
