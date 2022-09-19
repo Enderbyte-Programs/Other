708af48e6987899e4b0be417625319c7f347d20e
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-09-19 08:39:42 -0700

    Added arguments (run with --help)

--- ytdn.py
+++ ytdn.py
@@ -1,10 +1,55 @@
-import subprocess
-import pytube
-import glob
-import sys
-import os
-from awesome_progress_bar import ProgressBar
-import random
+#!/usr/bin/env python3
+try:
+    import subprocess
+    import pytube
+    import glob
+    import sys
+    import os
+    from awesome_progress_bar import ProgressBar
+    import random
+    import shutil
+except ImportError:
+    print("FATAL ERROR: Some modules could not be found.")
+    sys.exit(1)
+try:
+    args = sys.argv[1:]
+    if len(args) == 0 or (args[0][0:24] != "https://www.youtube.com/" and "-d" not in args and "--help" not in args and not "--empty" in args):
+        raise IndexError("ERROR")
+    if "--help" in args:
+        print("Youtube Compilation Maker v0.2 (c) 2022 Enderbyte Programs LLC")
+        print("--help      : Show this menu")
+        print("-d          : Does not download videos, only extracts and compiles")
+        print("-c          : Does not compile videos, only downloads (using in conjunction with -d will recompile files)")
+        print("--nowaste   : Cleans up extra files at the end of session")
+        print("--clean     : Clean files before downloading/compiling")
+        print("--cleanc    : Clean extracted files only (useful in conjunction with -d)")
+        print("--empty     : Empty files then exit")
+        sys.exit()
+    if "-d" in args:
+        _dlv = False
+    else:
+        _dlv = True
+    if "-c" in args:
+        _exv = False
+    else:
+        _exv = True
+    
+except IndexError:
+    print("ERROR: Please provide url to channel (eg. python3 ytdn.py <channel url> <args (optional)>) or run with --help for help")
+    sys.exit()
+
+rsz = subprocess.run(["ffmpeg"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+if rsz.returncode != 0 and rsz.returncode != 1:
+    print("Did you forget to install ffmpeg?")
+    sys.exit(1)
+
+if "--clean" in args:
+    [os.remove(l) for l in glob.glob("*.mp4")+glob.glob("*.txt")]#I love python
+elif "--cleanc" in args:
+    [os.remove(l) for l in glob.glob("*s.mp4")+glob.glob("*x.mp4")+glob.glob("*.txt")+glob.glob("*t.mp4")]
+elif "--empty" in args:
+    [os.remove(l) for l in glob.glob("*.mp4")+glob.glob("*.txt")]
+    sys.exit()
 
 def get_length(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
@@ -14,9 +59,9 @@ def get_length(filename):
         stderr=subprocess.STDOUT)
     return float(result.stdout)
 
-_dlv = True
+
 if _dlv:# Only downloading when conditional. Downloading takes over an hour.
-    c = pytube.Channel("https://www.youtube.com/channel/UCQpozDPRr27sYJz8E_IfhWg")
+    c = pytube.Channel(args[0])
     inc = 0
     bar = ProgressBar(len(c.video_urls)*2,prefix="Downloading",suffix="Preparing",bar_length=200,spinner_type="s")
     for video in c.video_urls:
@@ -30,14 +75,15 @@ if _dlv:# Only downloading when conditional. Downloading takes over an hour.
         except:
             bar.iter()
             try:
-                os.remove(f"vid_{inc}.mp4")
+                os.remove(f"vid_{inc}.mp4")#Removing corrupt video if applicable
             except:
                 pass
             continue
         inc += 1
     bar.wait()
     bar.stop()
-_exv = True
+    if not _exv:
+        sys.exit() #No point in throwing filenotfound error
 if _exv:
     g = glob.glob("*.mp4")
     bar = ProgressBar(int(len(g)*2),prefix="Extracting",suffix="Preparing",bar_length=200,spinner_type="s")
@@ -55,7 +101,7 @@ if _exv:
 
     bar.wait()
     bar.stop()
-
+print("Verifying Files")
 #Removing empty files to prevent glitvhes
 fls = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(),f))]
 for file in fls:
@@ -64,7 +110,7 @@ for file in fls:
             os.remove(file)
         except:
             print("Failed to remove corrupt file")
-
+print("Creating list")
 lg = glob.glob("*x.mp4")#Filtering split videos only
 data = ""
 for vid in lg:
@@ -75,5 +121,22 @@ for vid in lg:
     data += "'\n"
 with open("mylist.txt","w+") as f:
     f.write(data)
-
+print("Compiling...")
 rmn2 = subprocess.run(["ffmpeg","-f","concat","-safe","0","-i","mylist.txt","output.mp4"],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+if "--nowaste" in args:
+    print("Cleaning...")
+    if not os.path.isdir(".output"):
+        os.mkdir(".output")
+    shutil.copyfile("output.mp4",os.getcwd()+"/.output/output.mp4")
+    [os.remove(fl) for fl in glob.glob("*.mp4")]#LOL List comprehensions go BRR (excuse my use of 21st century humor)
+    try:
+        os.remove("mylist.txt")
+    except:
+        pass
+    shutil.copyfile(os.getcwd()+"/.output/output.mp4","output.mp4")
+    shutil.rmtree(".output")
+    try:
+        os.rmdir(".output")
+    except:
+        pass
+print("Done!")
